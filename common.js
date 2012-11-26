29094de26b802239adbe9f27deb7eda4b8b9f06a
Nat Weerawan <nat.wrw@gmail.com>
2012-11-26 11:37:51 +0700

    Add common.js

---
+++ common.js
@@ -0,0 +1,54 @@
+// LAYER MANAGER CREATER`
+window.CreateLayerManager = function(group, activeLayers) {
+    var funcs = { }
+    var manager = { funcs: funcs }
+    activeLayers = []
+
+    // CREATE FUNCTION WITH CAPTURED-VARIABLE
+    _.each(group, function(v, k) {
+        var year = k.split('-')[0];
+        var obj = {};
+        obj[k] = (function() {
+            var captured_obj = v
+              , ret = {
+                    layer: k.split('-').slice(1).join('-') || 'no',
+                    year: year,
+                    hide: function() {
+                        $(captured_obj).hide();
+                    },
+                    show: function() {
+                        $(captured_obj).show();
+                    },
+                    data: function() {
+                        return captured_obj;
+                    }
+                }
+            ret.hide();
+            return ret
+        })()
+        _.extend(funcs, obj)
+    })
+
+    manager.show = function(year, layerArr, callback) {
+        layerArr = _.union(layerArr, ["road", "area"])
+        var yearArr = [(year).toString(), "other"]
+        var items = _.filter(funcs, function(item, k) {
+            return _.contains(yearArr, item.year) && _.contains(layerArr, item.layer)
+        })
+
+        // HIDE ACTIVE LAYER
+        _.each(activeLayers, function(v) { v.hide() })
+
+        // SET NEW ACTIVE LAYER
+        activeLayers = items
+
+        _.each(items, function(item) { item.show(); })
+    }
+
+    manager.getAllLayers = function() {
+        return _.clone(["accident", "พท-เกษตรกรรม", "อาศัย", "พท-อนุรักษ์", "อุตสาหกรรมคลังสินค้า", "ราชการ", "อุตสาหกรรมไม่ก่อมลพิษ", "พท-โล่งเพื่อนันทนาการ"]);
+    }
+
+    return manager;
+
+} // LAYER MNGR CREATER
--- my2.js
+++ my2.js
@@ -17,65 +17,11 @@ $(function() {
             return i.id.split('_')[0] || 'other'
         })
 
-        // LAYER MANAGER CREATER`
-        window.CreateLayerManager = function(group, activeLayers) {
-            var funcs = { }
-            var manager = { funcs: funcs }
-            activeLayers = []
-
-            // CREATE FUNCTION WITH CAPTURED-VARIABLE
-            _.each(group, function(v, k) {
-                var year = k.split('-')[0];
-                var obj = {};
-                obj[k] = (function() {
-                    var captured_obj = v
-                      , ret = {
-                            layer: k.split('-').slice(1).join('-') || 'no',
-                            year: year,
-                            hide: function() {
-                                $(captured_obj).hide();
-                            },
-                            show: function() {
-                                $(captured_obj).show();
-                            },
-                            data: function() {
-                                return captured_obj;
-                            }
-                        }
-                    ret.hide();
-                    return ret
-                })()
-                _.extend(funcs, obj)
-            })
-
-            manager.show = function(year, layerArr, callback) {
-                layerArr = _.union(layerArr, ["road", "area"])
-                var yearArr = [(year).toString(), "other"]
-                var items = _.filter(funcs, function(item, k) {
-                    return _.contains(yearArr, item.year) && _.contains(layerArr, item.layer)
-                })
-
-                // HIDE ACTIVE LAYER
-                _.each(activeLayers, function(v) { v.hide() })
-
-                // SET NEW ACTIVE LAYER
-                activeLayers = items
-
-                _.each(items, function(item) { item.show(); })
-            }
-
-            manager.getAllLayers = function() {
-                return _.clone(["accident", "พท-เกษตรกรรม", "อาศัย", "พท-อนุรักษ์", "อุตสาหกรรมคลังสินค้า", "ราชการ", "อุตสาหกรรมไม่ก่อมลพิษ", "พท-โล่งเพื่อนันทนาการ"]);
-            }
-
-            return manager;
-
-        } // LAYER MNGR CREATER
-
         // CREATE LAYER MANAGER
         window.LayerManager = CreateLayerManager(gr, activeLayers1) // LAYER MANAGER
 
         var lays = LayerManager.getAllLayers()
+
         _.each(lays, function(v, k){
             var layerId = 'layer'+k
             window.elm = $('<input checked type="checkbox" name="myCheckbox" class="chooseLayer" value="'+ v +'" />'+v);
