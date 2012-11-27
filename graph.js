a547821b012e956cea6ec158121a75f05894e574
Nat Weerawan <nat.wrw@gmail.com>
2012-11-27 13:45:46 +0700

    Bind Event

--- graph.js
+++ graph.js
@@ -11,31 +11,28 @@ $(document).ready(function() {
         // CREATE LAYER MANAGER
         window.LayerManager2 = CreateLayerManager(gr2, activeLayers2) // LAYER MANAGER
         LayerManager2.show('graph', ['Factory', 'Accident', 'Event', 'GPP'])
-        bindEvent()
+
+        // BIND EVENT
+        bindEvent(LayerManager2.funcs['graph-Factory'].data(), jQuery('[id$="factoryBubble"]'))
+        bindEvent(LayerManager2.funcs['graph-GPP'].data(), jQuery('[id$="gppBubble"]'))
     })
 
 
-    function bindEvent() {
-        var graph_factory_data = LayerManager2.funcs['graph-Factory'].data();
-
-        var bubble = jQuery('[id$="factoryBubble"]')
-        _.each($('line', graph_factory_data), function(v, idx) {
+    function bindEvent(data, bubble) {
+        var bindBubble = function(v, idx) {
             $(v).hover(function(e) {
                 var $this = $(this);
                 bubble.hide()
                 e.stopPropagation()
                 bubble.eq(idx).show()
             })
+        }
+        _.each($('line', data), function(v, idx) {
+                bindBubble(v, idx)
         })
 
-        _.each($('circle', graph_factory_data), function(v, idx) {
-            $(v).hover(function(e) {
-                var $this = $(this);
-                $this.css({'opacity': 1.0, 'cursor': 'pointer'});
-                bubble.hide()
-                e.stopPropagation()
-                bubble.eq(idx).show()
-            })
+        _.each($('circle', data), function(v, idx) {
+                bindBubble(v, idx)
         })
     }
 
