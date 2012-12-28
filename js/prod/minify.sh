052f6f3236bfd356df6f188c8d6d51ba7095a553
Nat Weerawan <nat.wrw@gmail.com>
2012-12-28 17:40:10 +0700

    Add minify command

---
+++ js/prod/minify.sh
@@ -0,0 +1,4 @@
+yuicompressor ../../common.js -o common.min.js --charset utf-8 --verbose
+yuicompressor ../../common.js -o common.min.js --charset utf-8 --verbose
+yuicompressor ../../map.js -o map.min.js --charset utf-8 --verbose
+yuicompressor ../../map.js -o map.min.js --charset utf-8 --verbose
