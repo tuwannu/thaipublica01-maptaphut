f8cec4b2b6f7ab82a745f67554f131f507a2215d
Nat Weerawan <nat.wrw@gmail.com>
2013-01-02 16:48:16 +0700

    move minify.sh directory

--- js/prod/minify.sh
+++
@@ -1,4 +0,0 @@
-yuicompressor ../../common.js -o common.min.js --charset utf-8 --verbose
-yuicompressor ../../common.js -o common.min.js --charset utf-8 --verbose
-yuicompressor ../../map.js -o map.min.js --charset utf-8 --verbose
-yuicompressor ../../map.js -o map.min.js --charset utf-8 --verbose
---
+++ minify.sh
@@ -0,0 +1,4 @@
+yuicompressor common.js -o js/prod/common.min.js --charset utf-8 --verbose
+yuicompressor data.js -o js/prod/data.min.js --charset utf-8 --verbose
+yuicompressor map.js -o js/prod/map.min.js --charset utf-8 --verbose
+# yuicompressor map.js -o js/prod/map.min.js --charset utf-8 --verbose
